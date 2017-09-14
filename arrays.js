a9788b78ac07346ceee20f885b58cb38276b9bdb
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-14 21:46:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,6 @@
 var chocolateBars =["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  console.log("something something")
   return [element, ...array]
 }
 
